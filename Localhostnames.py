e6aad201f76b26aa2cd8155487082f459de1b737
Joachim <joagamed@gmail.com>
2018-05-04 15:28:28 +0200

    Create Localhostnames.py

---
+++ Localhostnames.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+with open('local-host-names','a+r') as f:
+	f_contents = f.read()
+	print(f_contents)
+	
+	f_contents.write("testdomein.nl"
+
+#with open('test.txt', 'w') as file:
+#	file.write(f_contents)
+	
