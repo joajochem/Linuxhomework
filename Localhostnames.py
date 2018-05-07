0a8a7068b89489544a1477df9a9a45d72b9a61ec
Joachim <joagamed@gmail.com>
2018-05-07 14:05:36 +0200

    Update Localhostnames.py

--- Localhostnames.py
+++ Localhostnames.py
@@ -5,7 +5,7 @@ with open('local-host-names','a+r') as f:
 	f_contents = f.read()
 	print(f_contents)
 	
-	f_contents.write("testdomein.nl"
+	f_contents.write("testdomein.nl")
 
 #with open('test.txt', 'w') as file:
 #	file.write(f_contents)
